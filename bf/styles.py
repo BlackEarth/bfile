98bf15c4723c128876b4310f32a10f413c1e182e
Sean Harrison <sah@blackearth.us>
2017-03-01 12:15:06 -0600

    add to the Styles object the ability to handle @font-face rules. [Does it really make sense to recreate the entire functionality of cssutils in plain Python? to what end? (1) supporting SCSS directives like @extends. (2) using built-in data structures is better. Eventually the bf.css / bf.scss / bf.stylesheet object should support everything that CSS and SCSS support. Even better: create a stylesheet object that supports a much broader range of general capabilities.]

--- bf/styles.py
+++ bf/styles.py
@@ -9,6 +9,59 @@ from bl.dict import Dict
 from bl.string import String
 
 class Styles(Dict):
+
+    @classmethod
+    def styleProperties(Class, style):
+        """return a properties dict from a given cssutils style
+        """
+        properties = Dict()
+        for property in style.getProperties(all=True):
+            stylename = property.name + ':'
+            properties[stylename] = property.value
+            if property.priority != '':
+                properties[stylename] = ' !'+property.priority
+        return properties
+
+    @classmethod
+    def from_css(Class, csstext, encoding=None, href=None, media=None, title=None, validate=None):
+        """parse CSS text into a Styles object, using cssutils
+        """
+        styles = Class()
+        cssStyleSheet = cssutils.parseString(csstext, encoding=encoding, href=href, media=media, title=title, validate=validate)
+        for rule in cssStyleSheet.cssRules:
+            if rule.type==cssutils.css.CSSRule.FONT_FACE_RULE:
+                if styles.get('@font-face') is None: styles['@font-face'] = []
+                styles['@font-face'].append(Class.styleProperties(rule.style))
+            
+            elif rule.type==cssutils.css.CSSRule.IMPORT_RULE:
+                if styles.get('@import') is None: styles['@import'] = []
+                styles['@import'].append(Dict(href=rule.href))              # no support yet for media queries
+            
+            elif rule.type==cssutils.css.CSSRule.NAMESPACE_RULE:
+                pass
+            
+            elif rule.type==cssutils.css.CSSRule.MEDIA_RULE:
+                pass
+            
+            elif rule.type==cssutils.css.CSSRule.PAGE_RULE:
+                pass
+            
+            elif rule.type==cssutils.css.CSSRule.STYLE_RULE:
+                for selector in rule.selectorList:
+                    sel = selector.selectorText
+                    if sel not in styles:
+                        styles[sel] = Class.styleProperties(rule.style)
+            
+            elif rule.type==cssutils.css.CSSRule.COMMENT:
+                pass
+            
+            elif rule.type==cssutils.css.CSSRule.VARIABLES_RULE:
+                pass
+            
+            else:
+                log.warning("Unknown rule type: %r" % rule.cssText)
+
+        return styles
     
     @classmethod    
     def render(c, styles, margin='', indent='\t'):
@@ -46,45 +99,3 @@ class Styles(Dict):
             s += '\n'
         return s
 
-    @classmethod
-    def from_css(Class, csstext, encoding=None, href=None, media=None, title=None, validate=None):
-        """parse CSS text into a Styles object, using cssutils"""
-        styles = Class()
-        cssStyleSheet = cssutils.parseString(csstext, encoding=encoding, href=href, media=media, title=title, validate=validate)
-        for rule in cssStyleSheet.cssRules:
-            if rule.type==cssutils.css.CSSRule.FONT_FACE_RULE:
-                pass
-            
-            elif rule.type==cssutils.css.CSSRule.IMPORT_RULE:
-                pass
-            
-            elif rule.type==cssutils.css.CSSRule.NAMESPACE_RULE:
-                pass
-            
-            elif rule.type==cssutils.css.CSSRule.MEDIA_RULE:
-                pass
-            
-            elif rule.type==cssutils.css.CSSRule.PAGE_RULE:
-                pass
-            
-            elif rule.type==cssutils.css.CSSRule.STYLE_RULE:
-                for selector in rule.selectorList:
-                    sel = selector.selectorText
-                    if sel not in styles:
-                        styles[sel] = Dict()
-                    for property in rule.style.getProperties(all=True):
-                        stylename = property.name + ':'
-                        styles[sel][stylename] = property.value
-                        if property.priority != '':
-                            styles[sel][stylename] = ' !'+property.priority
-            
-            elif rule.type==cssutils.css.CSSRule.COMMENT:
-                pass
-            
-            elif rule.type==cssutils.css.CSSRule.VARIABLES_RULE:
-                pass
-            
-            else:
-                log.warning("Unknown rule type: %r" % rule.cssText)
-
-        return styles
