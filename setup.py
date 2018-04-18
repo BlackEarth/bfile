29a669302c7215339a68ab928708bae1d5d411e6
Sean Harrison <sah@bookgenesis.com>
2018-04-18 15:41:40 -0500

    1.8.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 config = {
   "name": "bf",
-  "version": "1.8.1",
+  "version": "1.8.2",
   "description": "File interface library",
   "url": "https://github.com/BlackEarth/bf",
   "author": "Sean Harrison",
